6e8c202c3712ff03bf7de4478fe95ee0ae69259e
hukla <hukla@snu.ac.kr>
2014-06-01 18:38:53 +0900

    added comments to host_itf.v

--- device/Host_IF/host_itf.v
+++ device/Host_IF/host_itf.v
@@ -32,15 +32,30 @@ module host_itf (
 	output host_sel;
 	
 	reg [15:0] x8800_0010, x8800_0020, x8800_0030, x8800_0032, x8800_0040, x8800_0042, x8800_0050, x8800_0072, x8800_0090, x8800_00A0, x8800_00A2, x8800_00B0, x8800_00C0, x8800_00D0, x8800_00E0, x8800_00F0;
+	/*
+		reg [15:0] x8800_0010 // TextLCD Controller
+		reg [15:0] x8800_0020 // LED Controller
+		reg [15:0] x8800_0030; // 7-Segment controller
+		reg [15:0] x8800_0032; // 7-Segment Data Register
+		reg [15:0] x8800_0040; // Dot Matrix Scan Register(read/write)
+		reg [15:0] x8800_0042;	// Dot Matrix Data Register(read/write)
+		reg [15:0] x8800_0050; // Buzzer Controller
+		wire [15:0] x8800_0062; // DIP Switch Data Register(read only)
+		reg [15:0] x8800_00F0; // host mode 제어 - 
+		
+	*/
 	
+	// counter register & wires
 	reg [15:0] x8800_0034, x8800_0054, x8800_0056;
 	wire [15:0] x8800_0058, x8800_005A;
+	// counter register & wires 끝
 	
 	wire [15:0] x8800_0062, x8800_0070, x8800_0080, x8800_0092;
 	reg [1:0] reg_sw;
 	reg V_SEL;
 	integer clk_cnt;
 
+	// input 제어
 	always @(posedge clk or negedge nRESET) begin
 		if (nRESET == 1'b0) begin
 			x8800_0010 <= 16'b0;
@@ -87,8 +102,9 @@ module host_itf (
 				else if (reg_sw == 2'b10) x8800_00F0 <= ~x8800_00F0;
 			end
 		end
-	end
+	end // input 제어 끝
 	
+	// output 제어
 	always @(posedge clk or negedge nRESET) begin
 		if (nRESET == 1'b0) begin
 			HDO <= 16'b0;
@@ -122,22 +138,26 @@ module host_itf (
 				endcase
 			end
 		end
-	end
-	
+	end // output 제어 끝
+		
+	// CLCD_ctrl_Reg(Character LCD Control Register) at 0x8800_0010
 	assign CLCD_RS  = x8800_0010[10];
 	assign CLCD_RW  = x8800_0010[9];
 	assign CLCD_E   = x8800_0010[8];
 	assign CLCD_DQ  = x8800_0010[7:0];
 	
-	assign LED_D = x8800_0020[7:0];
+	assign LED_D = x8800_0020[7:0]; // LED_Ctrl_Reg (active high)
+	// 7-Segment Controller
+	assign SEG_COM  = ~x8800_0030[5:0]; // SEG_Sel_Reg (active low)
+	assign SEG_DATA = x8800_0032[7:0]; // SEG_Data_Reg (active high)
+	// 7-Segment Controller 끝
+
+	// Dot Matrix 
+	assign DOT_SCAN = x8800_0040[9:0]; // Dot_Scan_Reg (active high)
+	assign DOT_DATA = x8800_0042[6:0]; // Dot_Data_Reg (active high)
 	
-	assign SEG_COM  = ~x8800_0030[5:0];
-	assign SEG_DATA = x8800_0032[7:0];
-	
-	assign DOT_SCAN = x8800_0040[9:0];
-	assign DOT_DATA = x8800_0042[6:0];
-	
-	assign Piezo    = (x8800_0050[0] == 1'b1) ? 1'b1 : 1'b0;
+
+	assign Piezo    = (x8800_0050[0] == 1'b1) ? 1'b1 : 1'b0; // Buzzer_Ctrl_Reg (active high) -- buzzer enable bit
 	
 	assign x8800_0070 = (nRESET == 1'b1) ? {12'b0, PUSH_RD} : 16'b0;
 	assign PUSH_LD = x8800_0072[3:0];
@@ -146,8 +166,9 @@ module host_itf (
 	assign x8800_0080 = (nRESET == 1'b1) ? {12'b0, ~PUSH_SW} : 16'b0;
 	assign x8800_0092 = (nRESET == 1'b1) ? {10'b0, 6'b101010} : 16'b0;
 	
-	assign host_sel = x8800_00F0[0];
+	assign host_sel = x8800_00F0[0]; // 0이면 FPGA, 1이면 CPU
 	
+	/* counter controller */
 	assign COUNTER_SET = (x8800_0034[0] == 1'b1) ? 1'b1 : 1'b0;
 	assign COUNTER_START = x8800_0034[8];
 	assign COUNTER_DATA_INPUT = {x8800_0056, x8800_0054};
